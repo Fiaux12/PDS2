fa9f642a575e4a28f3e895549794aa90dd7fa7f4
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-05-14 14:07:39 -0300

    Update compilar.sh

--- exercicios/simulador/compilar.sh
+++ exercicios/simulador/compilar.sh
@@ -6,5 +6,10 @@ cd ..
 cd core-simulador-cpp
 g++ -c *.cpp 
 cd ..
-g++ principal\\main.cpp acessorios-cpp\\*.o core-simulador-cpp\\*.o
+cd personagens-cpp
+g++ -c *.cpp 
+cd ..
+g++ principal\\main.cpp acessorios-cpp\\*.o core-simulador-cpp\\*.o personagens-cpp\\*.o -o programa
+
+#Usei está linha para executar a simulação. Não consegui usar o bash para executar o script acima. 
 #g++ -I principal/ -I acessorios-cpp/ -I core-simulador-cpp/ -I personagens-cpp/ principal/*.cpp acessorios-cpp/*.cpp core-simulador-cpp/*.cpp personagens-cpp/*.cpp -o programa
